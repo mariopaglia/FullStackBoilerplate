92e0f0fc20187ebf2e19169a62f55ced3caf47e3
Mario Paglia <mpagliajr@gmail.com>
2024-01-01 14:37:19 -0300

    implement postgres in backend configuration

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
+  type: (process.env.DB_TYPE as 'mysql') || 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
