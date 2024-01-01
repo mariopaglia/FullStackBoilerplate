fe4266ae9bd05065b5708e33f63d6e8c0bb808c5
Mario Paglia <mpagliajr@gmail.com>
2024-01-01 15:44:17 -0300

    fix: port listen express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,17 +3,23 @@ import 'reflect-metadata';
 import { AppDataSource } from './data-source';
 import router from './routes';
 
-AppDataSource.initialize().then(() => {
-  console.log('Database connected!');
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Database connected!');
 
-  const app = express();
-  const port = 3333;
+    const app = express();
+    const port = 3333;
 
-  app.use(express.json());
-  app.use(router);
+    app.use(express.json());
+    app.use(router);
 
-  app.listen(port, () => console.log('Server is running!'));
-  
-}).catch((error) => {
-  console.log('Error connecting to database: ', error);
-});
+    app.listen(
+      {
+        port: process.env.PORT || port,
+      },
+      () => console.log(`Server is running in port ${port}!`)
+    );
+  })
+  .catch((error) => {
+    console.log('Error connecting to database: ', error);
+  });
