ade216017781e237e3c98eb51e154ba566b04bbd
Mario Paglia <mpagliajr@gmail.com>
2024-01-01 16:24:30 -0300

    fix: adjust port to listen express run

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,17 +8,12 @@ AppDataSource.initialize()
     console.log('Database connected!');
 
     const app = express();
-    const port = 3333;
+    const port = process.env.PORT || 3333;
 
     app.use(express.json());
     app.use(router);
 
-    app.listen(
-      {
-        port: process.env.PORT || port,
-      },
-      () => console.log(`Server is running in port ${port}!`)
-    );
+    app.listen(port, () => console.log(`Server is running in port ${port}!`));
   })
   .catch((error) => {
     console.log('Error connecting to database: ', error);
