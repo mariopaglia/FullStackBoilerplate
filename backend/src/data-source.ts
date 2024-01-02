f512020503e0796b6fe00750c9fc39b97583d9a4
Mario Paglia <mpagliajr@gmail.com>
2024-01-01 21:53:35 -0300

    build: configure Babel to build project

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export const AppDataSource = new DataSource({
   type: (process.env.DB_TYPE as 'mysql') || 'postgres',
   host: process.env.DB_HOST,
@@ -8,8 +10,8 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: Number(process.env.DB_PORT),
-  entities: ['src/entities/**/*.{js,ts}'],
-  migrations: ['src/migrations/**/*.{js,ts}'],
+  entities: [isDev ? 'src/entities/**/*.{js,ts}' : 'dist/entities/**/*.{js}'],
+  migrations: [isDev ? 'src/migrations/**/*.{js,ts}' : 'dist/migrations/**/*.{js}'],
   extra: {
     ssl: {
       rejectUnauthorized: false,
