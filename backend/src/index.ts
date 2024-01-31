31c7b1675f5d416cdfa73ff58d12b8645e17515d
Mario Paglia <mpagliajr@gmail.com>
2024-01-30 22:19:33 -0300

    chore: install cors in backend project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import 'reflect-metadata';
 import { AppDataSource } from './data-source';
@@ -9,6 +10,8 @@ AppDataSource.initialize()
 
     const app = express();
     const port = Number(process.env.PORT) || 3333;
+
+    app.use(cors());
 
     app.use(express.json());
     app.use(router);
